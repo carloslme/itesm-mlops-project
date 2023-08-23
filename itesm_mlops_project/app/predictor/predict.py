6f197b1756ea1ae63aabc59cc432ba97060009d6
carloslme <32799711+carloslme@users.noreply.github.com>
2023-08-22 21:34:38 -0600

    feat: add logging to app predict

--- itesm_mlops_project/app/predictor/predict.py
+++ itesm_mlops_project/app/predictor/predict.py
@@ -1,6 +1,16 @@
-import argparse
 import joblib
+import logging
 
+logger = logging.getLogger(__name__) # Indicamos que tome el nombre del modulo
+logger.setLevel(logging.DEBUG) # Configuramos el nivel de logging
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(module)s:%(levelname)s:%(message)s') # Creamos el formato
+
+file_handler = logging.FileHandler('predict.log') # Indicamos el nombre del archivo
+
+file_handler.setFormatter(formatter) # Configuramos el formato
+
+logger.addHandler(file_handler) # Agregamos el archivo
 class ModelPredictor:
     """
     A class to load a trained machine learning model and make predictions on new data.
@@ -24,6 +34,7 @@ class ModelPredictor:
             model_path (str): Path to the trained model file (joblib format).
         """
         self.model = joblib.load(model_path)
+        logger.debug("Model loaded!")
 
     def predict(self, new_data):
         """
@@ -36,16 +47,3 @@ class ModelPredictor:
             Predicted outputs from the model.
         """
         return self.model.predict(new_data)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Model Predictor')
-    parser.add_argument('model_path', type=str, help='Path to the trained model file')
-    parser.add_argument('new_data', type=str, help='Path to the file containing new data for prediction')
-    args = parser.parse_args()
-
-    predictor = ModelPredictor(args.model_path)
-
-    new_data = args.new_data
-
-    predictions = predictor.predict(new_data)
-    print(predictions)
